9058248b347f330bf54bde25584169f2d9b07513
omartinh <ojkkkr@NONAME.local>
2019-02-01 18:53:15 +0100

    commit url

--- Practica-1/wiki/server.js
+++ Practica-1/wiki/server.js
@@ -5,6 +5,6 @@ console.log("Arrancando servidor...")
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.end(req.url);
   console.log("Peticion atendida")
 }).listen(8080);
